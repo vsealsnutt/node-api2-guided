6a02959a69c3f8b62e543f545f547aa9eb35dc1d
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-02-23 22:56:13 -0500

    followed guided video to use express routers

---
+++ api/dogs/dogs-router.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Dog = require('./dogs-model');
+
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    Dog.find()
+      .then(dogs => {
+        res.status(200).json(dogs);
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: 'Error retrieving the dogs' });
+      });
+  });
+
+  module.exports = router;
